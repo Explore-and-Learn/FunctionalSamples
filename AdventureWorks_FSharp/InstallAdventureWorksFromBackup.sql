dcfbfc4aa0fdd367515e9829d630aaddaca82a16
marty-hope <marty.hope@gmail.com>
2016-03-06 12:03:28 -0800

    Add AdventureWorks backup and install script

---
+++ AdventureWorks_FSharp/InstallAdventureWorksFromBackup.sql
@@ -0,0 +1,8 @@
+﻿USE [master]
+
+--replace From disk value with path to backup
+RESTORE DATABASE AdventureWorks2014
+FROM disk= 'C:\Program Files\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQL\Backup\AdventureWorks2014.bak'
+WITH MOVE 'AdventureWorks2014_data' TO 'C:\Program Files\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQL\DATA\AdventureWorks2014.mdf',
+MOVE 'AdventureWorks2014_Log' TO 'C:\Program Files\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQL\DATA\AdventureWorks2014.ldf'
+,REPLACE
