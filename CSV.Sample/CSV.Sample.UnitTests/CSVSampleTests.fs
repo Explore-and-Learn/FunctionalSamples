a3e5bda6b9366324e74a53c3a76f202cf94b1b84
marty-hope <marty.hope@gmail.com>
2016-03-05 22:56:43 -0800

    Add csv type provider with examples

---
+++ CSV.Sample/CSV.Sample.UnitTests/CSVSampleTests.fs
@@ -0,0 +1,19 @@
+﻿namespace CSV.Sample.UnitTests
+open System
+open NUnit.Framework
+open System.Linq
+
+[<TestFixture>]
+type CSVSampleTests() = 
+
+    [<Test>]
+    member x.TestCSVHeaders() =
+        use sample = new CSV.Sample.CSVSample.sampleCsv()
+        Assert.IsTrue(sample.Headers.IsSome)
+
+  
+    [<Test>]
+    member x.TestCsvSort() =
+        let sortedRows : CSV.Sample.CSVSample.bookData[] = CSV.Sample.CSVSample.parseCsv
+        Assert.IsTrue(sortedRows.[0].author.Contains("Timothy"))
+
---
+++ CSV.Sample/CSVSample.fs
@@ -0,0 +1,29 @@
+﻿namespace CSV.Sample
+
+module CSVSample =
+    open System
+    open FSharp.Data
+
+    type bookData  = {
+        author: string
+        reviewDate : DateTime
+        isbn : int
+    }
+
+    type sampleCsv = CsvProvider<"Sample.csv">
+
+    let parseCsv =
+        use sample = new sampleCsv()
+
+        query {
+            for s in sample.Rows do
+            sortByDescending s.AUTHOR
+            select { 
+                    author = s.AUTHOR
+                    reviewDate = s.REVIEW_DATE
+                    isbn = s.ISBN
+                }
+        }
+        |> Seq.toArray
+
+
