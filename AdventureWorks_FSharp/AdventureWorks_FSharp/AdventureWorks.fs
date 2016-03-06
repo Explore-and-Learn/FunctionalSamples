0216be166e14ca97b080224d1aecf528856a511b
marty-hope <marty.hope@gmail.com>
2016-03-06 11:37:16 -0800

    Add Visual Studio Project that illustrates use of F# SQL Type provider

---
+++ AdventureWorks_FSharp/AdventureWorks_FSharp/AdventureWorks.fs
@@ -0,0 +1,33 @@
+﻿namespace AdventureWorks_FSharp
+
+
+module AdventureWorks =
+    
+    open System
+    open FSharp.Data
+    open System.Configuration
+
+    type Product = {
+        productName : string
+        productNumber : string
+        productId : int
+        listPrice: decimal
+    }
+
+    [<Literal>]
+    let productsQuery = "Select * from [Production].[Product]"
+
+    [<Literal>]
+    let connectionString = "Server=.;Database=AdventureWorks2014;Trusted_Connection=True;"
+    
+    let getProducts =
+        let query = new SqlCommandProvider<productsQuery, connectionString>()
+        query.Execute() 
+            |> Seq.map (fun product -> {
+                                        productName = product.Name
+                                        productNumber = product.ProductNumber
+                                        productId = product.ProductID
+                                        listPrice = product.ListPrice
+                                })
+
+       
